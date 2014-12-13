67007e08bdf16961c9e41627271d3ef5c9b19b3f
Zubair Desai <zubair.desai@gmail.com>
2014-12-13 10:11:25 -0800

    Solve lesson 2

--- 02_occurrences.rb
+++ 02_occurrences.rb
@@ -1,4 +1,5 @@
 require 'pp'
+require_relative 'curriculum'
 
 # Require the Ruby file
 #
@@ -28,3 +29,13 @@ require 'pp'
 #       g5 on 2013-06-08
 #       g6 on 2013-07-08
 #       etc...
+
+CURRICULUM[:units].each do |unit|
+  puts unit[:name]
+  unit[:lessons].each do |lesson|
+    puts "  #{lesson[:name]}"
+    lesson[:occurrences].each do |key, value|
+      puts "    #{value[:cohort][:name]} on #{key}"
+    end
+  end
+end
