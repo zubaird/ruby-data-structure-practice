6bf936062176941c09520970db0f8c55836609c8
Zubair Desai <zubair.desai@gmail.com>
2014-12-14 18:41:02 -0800

    complete lesson 05

--- 05_unit_comments.rb
+++ 05_unit_comments.rb
@@ -1,4 +1,5 @@
 require 'pp'
+require_relative 'curriculum'
 
 # Require the Ruby file
 #
@@ -9,3 +10,19 @@ require 'pp'
 #   Orientation - 9
 #   Rails Basics - 15
 #   CRUD - 12
+
+CURRICULUM[:units].each do |unit|
+  @total_comments = []
+
+  unit[:lessons].each do |lesson|
+    @comment_counter = []
+
+    lesson[:occurrences].each do |key, value|
+      value[:comments].each do |comment_details|
+        @comment_counter << comment_details[:comment]
+      end
+    end
+    @total_comments << @comment_counter.size
+  end
+  puts "#{unit[:name]} - #{@total_comments.inject(0, :+)}"
+end
