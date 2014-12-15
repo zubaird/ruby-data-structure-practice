fddd2a1fb37d33eb95915e912a9d1f222e758634
Zubair Desai <zubair.desai@gmail.com>
2014-12-14 17:20:21 -0800

    Solve lesson 04

--- 04_lesson_comments.rb
+++ 04_lesson_comments.rb
@@ -1,4 +1,5 @@
 require 'pp'
+require 'json'
 
 # Require json and parse the json file
 #
@@ -20,3 +21,26 @@ require 'pp'
 #     Read - 1
 #     Update - 6
 #     Delete - 2
+
+
+file = open("curriculum.json")
+json = file.read
+
+curriculum = JSON.parse(json)
+
+curriculum["units"].each do |unit|
+  puts unit["name"]
+
+  unit["lessons"].each do |lesson|
+    @comment_count = []
+    @lesson_name = lesson["name"]
+
+    lesson["occurrences"].each do |key, value|
+      value["comments"].each do |comment|
+        @comment_count << comment["user"]
+      end
+    end
+
+    puts "  #{@lesson_name} - #{@comment_count.size}"
+  end
+end
